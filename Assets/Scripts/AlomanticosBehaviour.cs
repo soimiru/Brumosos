4e19db91c048e6383d83e33ddcd22171152dc38a
Manuel <m.rodriguezb.2018@alumnos.urjc.es>
2021-12-09 17:13:37 +0100

    cambio en nombre y delete de archivo basura

--- Assets/Scripts/AlomanticosBehaviour.cs
+++ Assets/Scripts/AlomanticosBehaviour.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class AlomantesMAL : MonoBehaviour
+public class AlomanticosBehaviour : MonoBehaviour
 {
     public NavMeshAgent agent;
     private SimulationManager simManager;
